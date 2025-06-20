5ef84d16da8249e6fbb9c280a0ea75792a1b760a
Random-User-6 <jasyou59@gmail.com>
2025-06-20 09:30:08 -0700

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express();
 const upload = multer({ dest: 'uploads/' });
 const PORT = process.env.PORT || 3000;
 
+/**/
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+/**/
 app.use(express.static('public'));
 
 // Serve upload form
